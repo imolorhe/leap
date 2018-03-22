34f8a1e86ca4d5d7d14add5966a0f82e797ed39a
Samuel Imolorhe <samuelimolo4real@gmail.com>
2018-03-22 17:24:17 +0100

    Added Firebase to the project.

--- android/app/src/main/java/com/leap/MainApplication.java
+++ android/app/src/main/java/com/leap/MainApplication.java
@@ -3,6 +3,7 @@ package com.leap;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.imagepicker.ImagePickerPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new ImagePickerPackage(),
             new ReactNativeContacts(),
             new VectorIconsPackage()
