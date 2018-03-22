c1948dc3fbe090d26db35d03aa8865529df36231
Samuel Imolorhe <samuelimolo4real@gmail.com>
2018-03-22 17:50:15 +0100

    Added Firebase Database.

--- android/app/src/main/java/com/leap/MainApplication.java
+++ android/app/src/main/java/com/leap/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 import com.imagepicker.ImagePickerPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNFirebasePackage(),
+            new RNFirebaseDatabasePackage(),
             new ImagePickerPackage(),
             new ReactNativeContacts(),
             new VectorIconsPackage()
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import firebase from 'react-native-firebase';
 
 import configureStore from './redux';
 
 import { MainNavigator } from './navigators';
+
+firebase.auth()
+  .signInAnonymouslyAndRetrieveData()
+  .then(credentials => {
+    if (credentials) {
+      console.tron.log(JSON.stringify(credentials.user.toJSON()));
+    }
+  });
+
 export class App extends Component {
   render() {
     return (
